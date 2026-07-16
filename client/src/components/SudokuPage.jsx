21c919f2bf8c14d541c96873136ee7aa586cae98
Om Garg <omgarg3000@gmail.com>
2026-07-16 19:32:39 +0530

    simple 3x3 sudoku implemented completely

--- client/src/components/SudokuPage.jsx
+++ client/src/components/SudokuPage.jsx
@@ -39,6 +39,7 @@ export const SudokuPage =()=>{
     const [inUse, setInUse]=useState("")
     const [board, setBoard]=useState({...boxes})
     const [puzzle, setPuzzle]=useState("")
+    const [result, setResult]=useState("in game")
 
     const boxClickHandle=(boxName)=>{
         if(boxName in puzzle) return
@@ -46,6 +47,7 @@ export const SudokuPage =()=>{
     }
 
     const boxInputHandle =(e)=>{
+        if(e.target.id in puzzle) return
         const input =e.target.value
 
         if (/^[1-9]?$/.test(input)){
@@ -65,29 +67,44 @@ export const SudokuPage =()=>{
         setPuzzle(finalBoard)
         setBoard({...boxes, ...finalBoard})
     }
+    
+    const submitHandle=()=>{
+        const checkSet = new Set()
+        for(const[key, val] of Object.entries(board)){
+            checkSet.add(val)
+        }
+        if(checkSet.size<8){
+            setResult("Not Solved")
+            return
+        }
+        setResult("Solved")
+    }
 
     return(<>
     <h2>Sudoku</h2>
     <div className="board">
         <div className="row">
-            <input className={`box ${inUse=="box1"?'selected':""}`} onClick={()=>boxClickHandle("box1")} value={board.box1} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box2"?'selected':""}`} onClick={()=>boxClickHandle("box2")} value={board.box2} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box3"?'selected':""}`} onClick={()=>boxClickHandle("box3")} value={board.box3} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box1"?'selected':""}`} id="box1" onClick={()=>boxClickHandle("box1")} value={board.box1} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box2"?'selected':""}`} id="box2" onClick={()=>boxClickHandle("box2")} value={board.box2} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box3"?'selected':""}`} id="box3" onClick={()=>boxClickHandle("box3")} value={board.box3} onChange={boxInputHandle}/>
         </div>
         <div className="row">
-            <input className={`box ${inUse=="box4"?'selected':""}`} onClick={()=>boxClickHandle("box4")} value={board.box4} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box5"?'selected':""}`} onClick={()=>boxClickHandle("box5")} value={board.box5} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box6"?'selected':""}`} onClick={()=>boxClickHandle("box6")} value={board.box6} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box4"?'selected':""}`} id="box4" onClick={()=>boxClickHandle("box4")} value={board.box4} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box5"?'selected':""}`} id="box5" onClick={()=>boxClickHandle("box5")} value={board.box5} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box6"?'selected':""}`} id="box6" onClick={()=>boxClickHandle("box6")} value={board.box6} onChange={boxInputHandle}/>
         </div>
         <div className="row">
-            <input className={`box ${inUse=="box7"?'selected':""}`} onClick={()=>boxClickHandle("box7")} value={board.box7} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box8"?'selected':""}`} onClick={()=>boxClickHandle("box8")} value={board.box8} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box9"?'selected':""}`} onClick={()=>boxClickHandle("box9")} value={board.box9} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box7"?'selected':""}`} id="box7" onClick={()=>boxClickHandle("box7")} value={board.box7} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box8"?'selected':""}`} id="box8" onClick={()=>boxClickHandle("box8")} value={board.box8} onChange={boxInputHandle}/>
+            <input className={`box ${inUse=="box9"?'selected':""}`} id="box9" onClick={()=>boxClickHandle("box9")} value={board.box9} onChange={boxInputHandle}/>
         </div>
     </div>
     <div>
         <button onClick={newGameHandle}>New Game</button>
         <button onClick={resetHandle}>Clear</button>
+        <button onClick={submitHandle}>Submit</button>
+        <br />
+        <h2>Result: {result}</h2>
     </div>
     </>)
 }
