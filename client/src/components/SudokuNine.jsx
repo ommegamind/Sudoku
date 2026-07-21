ea464caec13d9afeddcd360c7c78d49ca8ac83fe
Om Garg <omgarg3000@gmail.com>
2026-07-21 12:28:43 +0530

    puzzle creator function created completely

--- client/src/components/SudokuNine.jsx
+++ client/src/components/SudokuNine.jsx
@@ -1,158 +1,152 @@
-import "./styles/SudokuNineStyle.css"
+import "./styles/SudokuNineStyle.css";
 
-export const SudokuNine=()=>{
+export const SudokuNine = () => {
+  const shuffle = (arr) => {
+    let i = arr.length - 1;
+    while (i > 0) {
+      let j = Math.floor(Math.random() * (i + 1));
+      let temp = arr[j];
+      arr[j] = arr[i];
+      arr[i] = temp;
 
-    const shuffle=(arr)=>{
-        let i=arr.length-1
-        while(i>0){
-            let j = Math.floor(Math.random()*(i+1))
-            let temp=arr[j]
-            arr[j]=arr[i]
-            arr[i]=temp
+      i--;
+    }
+    return arr;
+  };
 
-            i--;
-        }
-        return arr
+  const isValid = (board, row, col, num) => {
+    for (let testCol = 0; testCol < 9; testCol++) {
+      if (board[row][testCol] === num) {
+        return false;
+      }
     }
 
-    const isValid =(board, row, col, num)=>{
-        for(let testCol=0; testCol<9; testCol++){
-            if(board[row][testCol]===num){
-                return false
-            }
-        }
-
-        for(let testRow=0; testRow<9; testRow++){
-            if(board[testRow][col]===num){
-                return false
-            }
-        }
-
-        const startRow=Math.floor(row/3)*3
-        const startCol=Math.floor(col/3)*3
-
-        for(let boxRow=startRow; boxRow<startRow+3;boxRow++){
-            for(let boxCol=startCol; boxCol<startCol+3;boxCol++){
-                if(board[boxRow][boxCol]===num){
-                    return false
-                }
-            }
-        }
-        return true
+    for (let testRow = 0; testRow < 9; testRow++) {
+      if (board[testRow][col] === num) {
+        return false;
+      }
     }
 
-    const fillBoard=(board)=>{
-        for(let row=0; row<9;row++){
+    const startRow = Math.floor(row / 3) * 3;
+    const startCol = Math.floor(col / 3) * 3;
 
-            for(let col=0; col<9; col++){
-
-                if(board[row][col]===0){
-
-                    const randomArray=shuffle([1,2,3,4,5,6,7,8,9])
-
-                    for(const num of randomArray){
-                        
-                        if(isValid(board, row, col, num)){
-                            board[row][col]=num
-
-                            if(fillBoard(board)){
-                            return true
-                            }
-
-                        }
-
-                        board[row][col]=0
-                    }
-                    return false
-                }
-            }
+    for (let boxRow = startRow; boxRow < startRow + 3; boxRow++) {
+      for (let boxCol = startCol; boxCol < startCol + 3; boxCol++) {
+        if (board[boxRow][boxCol] === num) {
+          return false;
         }
-        return true
+      }
+    }
+    return true;
+  };
+
+  const fillBoard = (board) => {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (board[row][col] === 0) {
+          const randomArray = shuffle([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+
+          for (const num of randomArray) {
+            if (isValid(board, row, col, num)) {
+              board[row][col] = num;
+
+              if (fillBoard(board)) {
+                return true;
+              }
+            }
+
+            board[row][col] = 0;
+          }
+          return false;
+        }
+      }
+    }
+    return true;
+  };
+
+  const boardElement = [];
+  for (let i = 1; i < 10; i++) {
+    const row = [];
+    for (let j = 1; j < 10; j++) {
+      row.push(0);
+    }
+    boardElement.push(row);
+  }
+
+  fillBoard(boardElement);
+
+  const countSolutions = (boardCopy) => {
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        if (boardCopy[row][col] === 0) {
+          let solution = 0;
+
+          for (let num = 1; num < 10; num++) {
+            if (isValid(boardCopy, row, col, num)) {
+              boardCopy[row][col] = num;
+
+              solution += countSolutions(boardCopy);
+
+              boardCopy[row][col] = 0;
+            }
+          }
+          return solution;
+        }
+      }
+    }
+    return 1;
+  };
+
+  const createPuzzle = (board, gaps) => {
+    let gapsCreated = 0;
+
+    const index = [];
+
+    for (let row = 0; row < 9; row++) {
+      for (let col = 0; col < 9; col++) {
+        index.push([row, col]);
+      }
     }
 
+    const randomIndex = shuffle(index);
 
-    const boardElement=[]
-    for(let i=1;i<10;i++){
-        const row=[]
-        for(let j=1;j<10;j++){
-            row.push(0)
-        }
-        boardElement.push(row)
+    for (
+      let index = 0;
+      index < randomIndex.length && gapsCreated < gaps;
+      index++
+    ) {
+      const [row, col] = randomIndex[index];
+      const restoreVal = board[row][col];
+      board[row][col] = 0;
+      const copyBoard = board.map((row) => [...row]);
+
+      const solCount = countSolutions(copyBoard);
+      if (solCount === 1) {
+        gapsCreated += 1;
+      } else if (solCount > 1) {
+        board[row][col] = restoreVal;
+      }
     }
+  };
 
-    fillBoard(boardElement)
+  createPuzzle(boardElement, 40);
 
-    const countSolutions=(boardCopy)=>{
-        let solution=0
+  const boardBoxes = boardElement.map((row, rIndex) =>
+    row.map((num, cIndex) => {
+      return (
+        <input
+          key={`${rIndex}${cIndex}`}
+          className="box"
+          value={`${num > 0 ? num : " "}`}
+        />
+      );
+    }),
+  );
 
-        for(let row=0;row<9;row++){
-
-            for(let col=0;col<9;col++){
-
-                if(boardCopy[row][col]==0){
-
-                    for(let num=1; num<10;num++){
-
-                        if(isValid(boardCopy, row, col, num)){
-                            boardCopy[row][col]=num
-
-                            solution+=countSolutions(boardCopy)
-
-                            boardCopy[row][col]=0
-                        }
-                    }
-                    return 1
-                }
-            }
-        }
-        return solution
-    }
-
-    const createPuzzle=(board, gaps)=>{
-        let size=0
-
-        const index=[]
-
-        for(let row=0; row<9; row++){
-            for(let col=0; col<9; col++){
-                index.push([row, col])
-            }
-        }
-
-        const randomIndex= shuffle(index)
-        console.log(randomIndex)
-
-        while(size<gaps){
-            for(let index=0; index<randomIndex.length;index++){
-                const restoreVal=board[[randomIndex[index[0]]][randomIndex[index[1]]]]
-                const copyBoard= board.map((row)=>[...row])
-                copyBoard[[randomIndex[index[0]]][randomIndex[index[1]]]]=0
-
-                if(countSolutions(copyBoard)===1){
-                    board[[randomIndex[index[0]]][randomIndex[index[1]]]]=0
-                    size++
-                }
-            }
-            break
-        }
-        return board
-    }
-
-    createPuzzle(boardElement, 40)
-
-
-    const boardBoxes= boardElement.map((row, rIndex)=> row.map((num, cIndex)=>{
-        return(<input key={`${rIndex}${cIndex}`} 
-                      className="box"
-                      value={`${num}`}/>)
-    }))
-
-
-
-    return(<>
-        <h1>Sudoku 9x9</h1>
-        <div className="playBoard">
-        {boardBoxes}
-        </div>
-    </>)
-}
+  return (
+    <>
+      <h1>Sudoku 9x9</h1>
+      <div className="playBoard">{boardBoxes}</div>
+    </>
+  );
+};
--- client/src/components/SudokuPage.jsx
+++ client/src/components/SudokuPage.jsx
@@ -1,110 +1,167 @@
-import { useState } from "react"
-import "./styles/SudokuPageStyle.css"
+import { useState } from "react";
+import "./styles/SudokuPageStyle.css";
 
-export const SudokuPage =()=>{
+export const SudokuPage = () => {
+  const randomNumSetHandle = () => {
+    const nums = new Set();
+    while (nums.size < 9) {
+      nums.add(Math.floor(Math.random() * 9) + 1);
+    }
+    const numsArray = [...nums];
 
-    const randomNumSetHandle=()=>{
-        const nums=new Set()
-        while(nums.size<9){
-            nums.add(Math.floor(Math.random()*9)+1)
-        }
-        const numsArray=[...nums]
+    const tips = Math.floor(Math.random() * 4) + 1;
 
-        const tips=Math.floor(Math.random()*4)+1
-
-        const boardIndex=[]
-        while(boardIndex.length<=tips){
-            boardIndex.push(Math.floor(Math.random()*9)+1)
-        }
-        
-        const finalBoard={}
-        for(let i=0; i<=tips; i++){
-            finalBoard[`box${boardIndex[i]}`]=numsArray[boardIndex[i]-1]
-        }
-
-        return finalBoard
+    const boardIndex = [];
+    while (boardIndex.length <= tips) {
+      boardIndex.push(Math.floor(Math.random() * 9) + 1);
     }
 
-    const boxes={
-        "box1":"",
-        "box2":"",
-        "box3":"",
-        "box4":"",
-        "box5":"",
-        "box6":"",
-        "box7":"",
-        "box8":"",
-        "box9":""
-    }
-    const [inUse, setInUse]=useState("")
-    const [board, setBoard]=useState({...boxes})
-    const [puzzle, setPuzzle]=useState("")
-    const [result, setResult]=useState("in game")
-
-    const boxClickHandle=(boxName)=>{
-        if(boxName in puzzle) return
-        setInUse(boxName)
+    const finalBoard = {};
+    for (let i = 0; i <= tips; i++) {
+      finalBoard[`box${boardIndex[i]}`] = numsArray[boardIndex[i] - 1];
     }
 
-    const boxInputHandle =(e)=>{
-        if(e.target.id in puzzle) return
-        const input =e.target.value
+    return finalBoard;
+  };
 
-        if (/^[1-9]?$/.test(input)){
-            setBoard((prev)=>{return {...prev, [inUse]:input}})
-        }
-    }
+  const boxes = {
+    box1: "",
+    box2: "",
+    box3: "",
+    box4: "",
+    box5: "",
+    box6: "",
+    box7: "",
+    box8: "",
+    box9: "",
+  };
+  const [inUse, setInUse] = useState("");
+  const [board, setBoard] = useState({ ...boxes });
+  const [puzzle, setPuzzle] = useState("");
+  const [result, setResult] = useState("in game");
 
-    const resetHandle=()=>{
-        setBoard(()=>{
-            return{...boxes, ...puzzle}
-        })
-        setInUse("")
-    }
+  const boxClickHandle = (boxName) => {
+    if (boxName in puzzle) return;
+    setInUse(boxName);
+  };
 
-    const newGameHandle=()=>{
-        const finalBoard=randomNumSetHandle()
-        setPuzzle(finalBoard)
-        setBoard({...boxes, ...finalBoard})
-    }
-    
-    const submitHandle=()=>{
-        const checkSet = new Set()
-        for(const[key, val] of Object.entries(board)){
-            checkSet.add(val)
-        }
-        if(checkSet.size<8){
-            setResult("Not Solved")
-            return
-        }
-        setResult("Solved")
-    }
+  const boxInputHandle = (e) => {
+    if (e.target.id in puzzle) return;
+    const input = e.target.value;
 
-    return(<>
-    <h2>Sudoku</h2>
-    <div className="board">
+    if (/^[1-9]?$/.test(input)) {
+      setBoard((prev) => {
+        return { ...prev, [inUse]: input };
+      });
+    }
+  };
+
+  const resetHandle = () => {
+    setBoard(() => {
+      return { ...boxes, ...puzzle };
+    });
+    setInUse("");
+  };
+
+  const newGameHandle = () => {
+    const finalBoard = randomNumSetHandle();
+    setPuzzle(finalBoard);
+    setBoard({ ...boxes, ...finalBoard });
+  };
+
+  const submitHandle = () => {
+    const checkSet = new Set();
+    for (const [key, val] of Object.entries(board)) {
+      checkSet.add(val);
+    }
+    if (checkSet.size < 8) {
+      setResult("Not Solved");
+      return;
+    }
+    setResult("Solved");
+  };
+
+  return (
+    <>
+      <h2>Sudoku</h2>
+      <div className="board">
         <div className="row">
-            <input className={`box ${inUse=="box1"?'selected':""}`} id="box1" onClick={()=>boxClickHandle("box1")} value={board.box1} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box2"?'selected':""}`} id="box2" onClick={()=>boxClickHandle("box2")} value={board.box2} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box3"?'selected':""}`} id="box3" onClick={()=>boxClickHandle("box3")} value={board.box3} onChange={boxInputHandle}/>
+          <input
+            className={`box ${inUse == "box1" ? "selected" : ""}`}
+            id="box1"
+            onClick={() => boxClickHandle("box1")}
+            value={board.box1}
+            onChange={boxInputHandle}
+          />
+          <input
+            className={`box ${inUse == "box2" ? "selected" : ""}`}
+            id="box2"
+            onClick={() => boxClickHandle("box2")}
+            value={board.box2}
+            onChange={boxInputHandle}
+          />
+          <input
+            className={`box ${inUse == "box3" ? "selected" : ""}`}
+            id="box3"
+            onClick={() => boxClickHandle("box3")}
+            value={board.box3}
+            onChange={boxInputHandle}
+          />
         </div>
         <div className="row">
-            <input className={`box ${inUse=="box4"?'selected':""}`} id="box4" onClick={()=>boxClickHandle("box4")} value={board.box4} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box5"?'selected':""}`} id="box5" onClick={()=>boxClickHandle("box5")} value={board.box5} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box6"?'selected':""}`} id="box6" onClick={()=>boxClickHandle("box6")} value={board.box6} onChange={boxInputHandle}/>
+          <input
+            className={`box ${inUse == "box4" ? "selected" : ""}`}
+            id="box4"
+            onClick={() => boxClickHandle("box4")}
+            value={board.box4}
+            onChange={boxInputHandle}
+          />
+          <input
+            className={`box ${inUse == "box5" ? "selected" : ""}`}
+            id="box5"
+            onClick={() => boxClickHandle("box5")}
+            value={board.box5}
+            onChange={boxInputHandle}
+          />
+          <input
+            className={`box ${inUse == "box6" ? "selected" : ""}`}
+            id="box6"
+            onClick={() => boxClickHandle("box6")}
+            value={board.box6}
+            onChange={boxInputHandle}
+          />
         </div>
         <div className="row">
-            <input className={`box ${inUse=="box7"?'selected':""}`} id="box7" onClick={()=>boxClickHandle("box7")} value={board.box7} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box8"?'selected':""}`} id="box8" onClick={()=>boxClickHandle("box8")} value={board.box8} onChange={boxInputHandle}/>
-            <input className={`box ${inUse=="box9"?'selected':""}`} id="box9" onClick={()=>boxClickHandle("box9")} value={board.box9} onChange={boxInputHandle}/>
+          <input
+            className={`box ${inUse == "box7" ? "selected" : ""}`}
+            id="box7"
+            onClick={() => boxClickHandle("box7")}
+            value={board.box7}
+            onChange={boxInputHandle}
+          />
+          <input
+            className={`box ${inUse == "box8" ? "selected" : ""}`}
+            id="box8"
+            onClick={() => boxClickHandle("box8")}
+            value={board.box8}
+            onChange={boxInputHandle}
+          />
+          <input
+            className={`box ${inUse == "box9" ? "selected" : ""}`}
+            id="box9"
+            onClick={() => boxClickHandle("box9")}
+            value={board.box9}
+            onChange={boxInputHandle}
+          />
         </div>
-    </div>
-    <div>
+      </div>
+      <div>
         <button onClick={newGameHandle}>New Game</button>
         <button onClick={resetHandle}>Clear</button>
         <button onClick={submitHandle}>Submit</button>
         <br />
         <h2>Result: {result}</h2>
-    </div>
-    </>)
-}
+      </div>
+    </>
+  );
+};
